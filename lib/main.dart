d2ddc32d99c3228a98805a21a294694254b13f66
Alison <alison.asa@gmail.com>
2025-02-13 00:59:03 -0400

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          appBar: AppBar(
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundColor: Color.fromRGBO(233, 230, 248, 1),
+                child: Icon(Icons.person),
+              ),
+            ),
+            actions: [
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add),
+                    iconSize: 18,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(28, 28, 28, 1),
+                        iconColor: Colors.white),
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: IconButton(
+                  iconSize: 18,
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications_none),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(233, 230, 248, 1),
+                    iconColor: Color.fromRGBO(28, 28, 28, 1),
+                  ),
+                ),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text(
+                  "Manage Your Task",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
