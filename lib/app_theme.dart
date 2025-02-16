fd3261fd0b5791c26a6dd106fc4f227e01676d84
Alison <alison.asa@gmail.com>
2025-02-16 02:23:23 -0400

    new things

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 
 const mainColor = Color.fromRGBO(233, 230, 248, 1);
 const secondaryColor = Color.fromRGBO(28, 28, 28, 1);
-
-ThemeData mainTheme = ThemeData(
-  primaryColor: mainColor,
-  iconButtonTheme: IconButtonThemeData(
-    style: ButtonStyle(
-      backgroundColor: WidgetStateProperty.all(secondaryColor), // Substitua
-      iconColor: WidgetStateProperty.all(Colors.white), // Substitua
-    ),
-  ),
-);
+const myPurpleColor = Color.fromRGBO(166, 160, 210, 1);
+const myBlackColor = Color.fromRGBO(29, 29, 29, 1);
+const myGreenColor = Color.fromRGBO(203, 216, 125, 1);
+const myGreyColor = Color.fromRGBO(235, 235, 235, 1);
